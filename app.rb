be28daa22e6acbcfcc77a45a828298c2c2f062ef
jin camou and Steven Galvin <jin@>
2017-04-18 12:02:05 -0700

    add instance variables, multipleforms using erb, update app.rb, clean up landing page to link to all pages

--- app.rb
+++ app.rb
@@ -2,7 +2,9 @@ require('sinatra')
 require('sinatra/reloader')
 
 get('/') do
- erb(:lakes)
+  @recipient = "Mt Hood Adventurer"
+  @sender = "From another Mt Hood Enthusiast"
+ erb(:landing)
 end
 
 get('/hood_lakes') do
@@ -10,5 +12,30 @@ get('/hood_lakes') do
 end
 
 get('/trillium') do
+  @recipient = "Mt Hood Adventurer"
+  @sender = "From another Mt Hood Enthusiast"
   erb(:trillium)
+end
+
+get('/greeting_form') do
+  erb(:form)
+end
+
+get('/greeting_card') do
+  @sender = params.fetch('sender')
+  @recipient = params.fetch('recipient')
+  erb(:greeting_card)
+end
+
+get('/camping_form') do
+  erb(:camping_form)
+end
+
+get('/camping_confirmation') do
+  @camper1 = params.fetch('camper1')
+  @camper2 = params.fetch('camper2')
+  @camper3 = params.fetch('camper3')
+  @campground = params.fetch('campground')
+  @equipment = params.fetch('equipment')
+  erb(:camping_confirmation)
 end
